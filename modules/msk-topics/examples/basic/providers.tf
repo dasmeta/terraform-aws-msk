7f732fe2e31b3874c902a39444356421274e2f95
sophie-dasmeta-com <sophie@dasmeta.com>
2026-06-16 12:14:20 +0400

    feat(DMVP-10097): remove provider.yaml

--- modules/msk-topics/examples/basic/providers.tf
+++
@@ -1,7 +0,0 @@
-provider "kafka" {
-  bootstrap_servers = split(",", var.bootstrap_brokers)
-  tls_enabled       = true
-  sasl_username     = var.sasl_username
-  sasl_password     = var.sasl_password
-  sasl_mechanism    = var.sasl_mechanism
-}
